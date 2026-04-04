ee0f90e46d2e1869b53b69976a6943bb2394ce36
Suvransu Sekhar Nayak <suvransu469@gmail.com>
2026-04-04 18:08:00 +0530

    Time: 0 ms (100.00%) | Memory: 52.8 MB (95.83%) - LeetSync

---
+++ 75-sort-colors/sort-colors.js
@@ -0,0 +1,28 @@
+/**
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var sortColors = function(nums) {
+    let count0=0;
+    let count1=0;
+    let count2=0;
+    for(let i=0;i<nums.length;i++){
+        if(nums[i]===0){
+         count0++
+        }
+        else if(nums[i]===1){
+            count1++
+        }
+        count2++
+    }
+    for(let i=0;i<count0;i++){
+        nums[i]=0
+    }
+    for(let i=count0;i<count0+count1;i++){
+        nums[i]=1
+    }
+    for(let i=count0+count1;i<nums.length;i++){
+        nums[i]=2
+    }
+}
+  
